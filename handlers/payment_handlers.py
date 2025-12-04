e1ed0abb72f3166d4db686c4041208fb2e81763d
Georgy <gosha.dvoryak@gmail.com>
2025-12-04 01:49:32 +0100

    fixing db 2

--- handlers/payment_handlers.py
+++ handlers/payment_handlers.py
@@ -342,7 +342,10 @@ def register_handlers(bot):
         try:
             # Get username from database
             user_info = get_user(user_id)
-            username = user_info["username"] if user_info and user_info.get("username") else "user"
+            if user_info and "username" in user_info.keys() and user_info["username"]:
+                username = user_info["username"]
+            else:
+                username = "user"
             
             # Build payment link
             customer_phone = ""
